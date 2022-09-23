f5f3530ed6d946d6cffa76667b2066255f60bc7c
walex <w.alex36@outlook.com>
2022-09-23 02:48:50 -0300

    first commit

---
+++ lib/src/core/services/dot_env/dot_env_service.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+class DotEnvService {
+  final Map<String, String> _map = {};
+  static DotEnvService instance = DotEnvService._();
+
+  DotEnvService._() {
+    _init();
+  }
+
+  void _init() {
+    final file = File('.env');
+
+    final envTxt = file.readAsStringSync();
+
+    for (var line in envTxt.split('\n')) {
+      final lineBreak = line.split('=');
+      _map[lineBreak[0]] = lineBreak[1];
+    }
+  }
+
+  String? operator [](String key) {
+    return _map[key];
+  }
+}
